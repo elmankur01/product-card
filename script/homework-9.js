a1499c2e5b4c9a6af7429e2fb2df535575bd6049
Your Name <your_email@whatever.com>
2026-05-05 17:45:18 +0300

    designed the footer

--- script/homework-9.js
+++ script/homework-9.js
@@ -18,7 +18,7 @@ if (subscribeForm) {
     alert("Спасибо за подписку!");
   });
 }
-// ========== МОДАЛЬНОЕ ОКНО (задание 5) ==========
+// ========== МОДАЛЬНОЕ ОКНО (задание 5) =========
 const modal = document.getElementById("modal");
 const registerBtn = document.getElementById("registerBtn");
 const closeModalBtn = document.getElementById("closeModalBtn");
